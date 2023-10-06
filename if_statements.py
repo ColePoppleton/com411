d9c163c6fde7e044f62a33f857c8b7850d80d635
HenriVIII1509 <62750489+HenriVIII1509@users.noreply.github.com>
2023-10-06 09:13:57 +0100

    Added code demonstrating an if statement

--- if_statements.py
+++ if_statements.py
@@ -1,1 +1,9 @@
 
+genres = ["adventure", "fantasy", "non-fiction", "sci-fi", "mystery"]
+
+book = input("What type of book is this? \n")
+if book in genres:
+    print(f"I like {book} books")
+    print("finished reading")
+else:
+    print("sorry not a valid genre")
