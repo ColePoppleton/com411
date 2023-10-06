5e8567d510d8b827e9f03b1608cae9e2f2cb71b5
HenriVIII1509 <62750489+HenriVIII1509@users.noreply.github.com>
2023-10-06 09:24:35 +0100

    Added code demonstrating conditional or

---
+++ multipule_conditions.py
@@ -0,0 +1,6 @@
+type_Of_Adventure = input("What type of adventure should I have?\n")
+
+if type_Of_Adventure == "scary" or type_Of_Adventure == "short":
+    print("Entering the dark forest")
+elif type_Of_Adventure == "safe" or type_Of_Adventure == "long":
+    print("Taking the safe route!")
