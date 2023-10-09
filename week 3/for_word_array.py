b243fba925630451d01a3c1476dc53606e159bd5
HenriVIII1509 <62750489+HenriVIII1509@users.noreply.github.com>
2023-10-09 17:45:17 +0100

    Added code to demonstrate the use of membership operators with a for loop.

---
+++ week 3/for_word_array.py
@@ -0,0 +1,7 @@
+phrase = input("What phrase do you want to print? \n")
+# variable for phrase to be broken into each character
+
+
+print("\noutputting...\n")
+for letter in phrase:
+    print(letter)
